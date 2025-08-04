e64ba35b1dc1682c9e72de87a27cc6f1a65e4073
ThanhVui <thanhvuikg100@gmail.com>
2025-08-04 10:53:10 +0700

    "Update: Model version 7"

--- utils/logging.py
+++
@@ -1,9 +0,0 @@
-# utils/logging.py
-import logging
-
-# --- LOGGING ---
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-    handlers=[logging.FileHandler("Logs/api_server.log"), logging.StreamHandler()]
-)
